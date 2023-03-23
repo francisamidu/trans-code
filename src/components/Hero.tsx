a6c8eebf389f8558c4c780736b6090adb48ea4f7
francisamidu <francisamidu124@gmail.com>
2023-03-23 14:24:29 +0200

    Working on hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <main className="my-20 flex flex-1 flex-col items-center justify-center text-center"></main>
+  );
+};
+
+export default Hero;
