c6317b60270c92265850b2ea97e0fbfd20f862ab
francisamidu <francisamidu124@gmail.com>
2023-03-23 15:24:39 +0200

    UI update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Github } from 'lucide-react';
+import { GithubIcon as Github } from 'lucide-react';
 import { APP_NAME } from '@/utils/getEnv';
 
 const Hero = () => {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center p-5 text-center">
-      <nav className="flex flex-row items-center justify-around md:mx-auto md:max-w-screen-lg">
-        <a href="/" className="self-center whitespace-nowrap text-xl font-bold">
+    <section className="h-full w-full flex-1">
+      <nav className="flex flex-row items-center justify-between py-4 md:mx-auto md:max-w-screen-lg">
+        <a href="/" className="text-xl font-bold">
           {APP_NAME}
         </a>
         <a
           href="https://www.github.com/francisamidu/language-x"
           target="_blank"
         >
-          <Github color="#111" size={20} />
+          <Github color="#111" size={25} />
         </a>
       </nav>
-    </main>
+    </section>
   );
 };
 
