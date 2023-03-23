53f4ff7e9bd83816ef2e6177ab846274d42d0743
francisamidu <francisamidu124@gmail.com>
2023-03-23 14:25:04 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
 import Hero from './components/Hero';
 const App = () => {
   return (
     <main className="min-h-[96vh] overflow-x-hidden bg-[#02000e]">
-      <ToastContainer />
       <Hero />
     </main>
   );
