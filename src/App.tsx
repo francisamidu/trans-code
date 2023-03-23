0f9967b4ee2d5b9f98d96076cd90e212b329ef1d
francisamidu <francisamidu124@gmail.com>
2023-03-23 16:30:36 +0200

    UI update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from './components/Hero';
 const App = () => {
   return (
-    <main className="min-h-[96vh] overflow-x-hidden ">
+    <main className="min-h-[96vh] overflow-x-hidden bg-[#f1f5f9]">
       <Hero />
     </main>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
   return (
-    <section className="h-full w-full flex-1 bg-blue-50">
+    <section className="h-full w-full flex-1 ">
       <nav className="flex flex-row items-center justify-between py-4 md:mx-auto md:max-w-screen-lg">
         <a href="/" className="text-xl font-bold text-eerie">
           {APP_NAME}
@@ -23,7 +23,7 @@ const Hero = () => {
           <Github color="#111" size={25} />
         </a>
       </nav>
-      <div className="my-4 mx-auto flex flex-col items-center text-center md:w-7/12">
+      <div className="my-7 mx-auto flex flex-col items-center text-center md:w-7/12">
         <h1 className="px-4 text-2xl font-bold capitalize md:px-0 md:text-6xl md:leading-[4rem]">
           {APP_SHORT_DESCRIPTION}
         </h1>
@@ -31,11 +31,11 @@ const Hero = () => {
           {APP_LONG_DESCRIPTION}
         </p>
       </div>
-      <form className="flex flex-row">
-        <div className="flex flex-col bg-white">
+      <form className="flex flex-col items-center justify-center">
+        <div className="flex flex-col rounded-t-md bg-white md:w-7/12">
           <label
             htmlFor="input"
-            className="w-full py-2 text-sm font-bold text-eerie"
+            className="w-full border-b-[1px] border-b-gray-100 p-2 text-sm font-bold text-gray-300"
           >
             Code
           </label>
@@ -44,7 +44,7 @@ const Hero = () => {
             value={input}
             onChange={(e) => setInput(e.target.value)}
             rows={4}
-            className="relative rounded-md p-2 shadow-sm outline-none md:w-1/2"
+            className="relative rounded-b-md p-2 shadow-sm outline-none "
             placeholder={
               'Write or paste the code you want to translate/convert here'
             }
